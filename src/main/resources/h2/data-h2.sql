7e43090381d7f9b58d6ca4c6c0aa940bb153707c
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-07-15 14:24:28 +0200

    fix: updated H2 scripts

--- src/main/resources/h2/data-h2.sql
+++ src/main/resources/h2/data-h2.sql
@@ -1,12 +1,13 @@
-INSERT INTO mocker.tag(id, "value") VALUES('398ddf77-8012-4767-84aa-87ad194d3ac9', 'EC');
-INSERT INTO mocker.tag(id, "value") VALUES('261af795-2652-401e-a9c2-dcc160041bf6', 'GPD');
-INSERT INTO mocker.tag(id, "value") VALUES('eaa80a30-2992-47fc-b607-4a248d03066b', 'ORGS ENROLLMENT');
-INSERT INTO mocker.tag(id, "value") VALUES('88513e6d-7229-43ea-96b5-f83ffd96d5f5', 'MAIN RULE');
+INSERT INTO mocker.resource_tag(id, "value") VALUES('398ddf77-8012-4767-84aa-87ad194d3ac9', 'EC');
+INSERT INTO mocker.resource_tag(id, "value") VALUES('261af795-2652-401e-a9c2-dcc160041bf6', 'GPD');
+INSERT INTO mocker.resource_tag(id, "value") VALUES('eaa80a30-2992-47fc-b607-4a248d03066b', 'ORGS ENROLLMENT');
+
+INSERT INTO mocker.rule_tag(id, "value") VALUES('88513e6d-7229-43ea-96b5-f83ffd96d5f5', 'MAIN RULE');
 
 
 
-INSERT INTO mocker.mock_resource(id, resource_id, subsystem_url, resource_url, http_method, "name", is_active)
-VALUES('70aa0abb-0001-0212-aa01-2cb1a0f00301', 'getgpdreportingorgsenrollmentapiv1organizations77777777777', 'gpd-reporting-orgs-enrollment/api/v1', 'organizations/77777777777', 'GET', 'Get enrolled organization with ID 77777777777', true);
+INSERT INTO mocker.mock_resource(id, subsystem_url, resource_url, http_method, "name", is_active)
+VALUES(4286553979, 'gpd-reporting-orgs-enrollment/api/v1', 'organizations/77777777777', 'GET', 'Get enrolled organization with ID 77777777777', true);
 
 
 
@@ -21,7 +22,7 @@ VALUES('26e05a1f-9621-4e24-a57d-28694ff30306', 'Content-Type', 'application/json
 
 
 INSERT INTO mocker.mock_rule(id, "name", "order", is_active, resource_id, response_id)
-VALUES('6c08a21c-6a92-4f6b-a1e1-bf68c4e099c9', 'Main rule', 1, true, '70aa0abb-0001-0212-aa01-2cb1a0f00301', '26e05a1f-9621-4e24-a57d-28694ff30306');
+VALUES('6c08a21c-6a92-4f6b-a1e1-bf68c4e099c9', 'Main rule', 1, true, 4286553979, '26e05a1f-9621-4e24-a57d-28694ff30306');
 
 
 
@@ -31,11 +32,11 @@ VALUES('6b0b003d-74f4-428e-b950-61f42e02bf07', 1, 'HEADER', 'STRING', 'ClientId'
 
 
 INSERT INTO mocker.mock_resource_tag(mock_resource_id, tag_id)
-VALUES('70aa0abb-0001-0212-aa01-2cb1a0f00301', '398ddf77-8012-4767-84aa-87ad194d3ac9');
+VALUES(4286553979, '398ddf77-8012-4767-84aa-87ad194d3ac9');
 INSERT INTO mocker.mock_resource_tag(mock_resource_id, tag_id)
-VALUES('70aa0abb-0001-0212-aa01-2cb1a0f00301', '261af795-2652-401e-a9c2-dcc160041bf6');
+VALUES(4286553979, '261af795-2652-401e-a9c2-dcc160041bf6');
 INSERT INTO mocker.mock_resource_tag(mock_resource_id, tag_id)
-VALUES('70aa0abb-0001-0212-aa01-2cb1a0f00301', 'eaa80a30-2992-47fc-b607-4a248d03066b');
+VALUES(4286553979, 'eaa80a30-2992-47fc-b607-4a248d03066b');
 
 
 
--- src/main/resources/h2/schema-h2.sql
+++ src/main/resources/h2/schema-h2.sql
@@ -1,12 +1,68 @@
-CREATE TABLE mocker.mock_resource (
+CREATE TABLE mocker.rule_tag (
 	id varchar NOT NULL,
-	resource_id varchar NOT NULL,
+	"value" varchar NOT NULL,
+	CONSTRAINT rule_tag_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE mocker.resource_tag (
+	id varchar NOT NULL,
+	"value" varchar NOT NULL,
+	CONSTRAINT resource_tag_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE mocker.archetype (
+	id varchar NOT NULL,
+	"name" varchar NOT NULL,
+	subsystem_url varchar NOT NULL,
+	resource_url varchar NOT NULL,
+	http_method varchar NOT NULL,
+	CONSTRAINT archetype_pk PRIMARY KEY (id),
+	CONSTRAINT archetype_u UNIQUE (subsystem_url, resource_url, http_method)
+);
+
+CREATE TABLE mocker.archetype_parameter (
+    archetype_id varchar NOT NULL,
+    "name" varchar NOT NULL,
+    "type" varchar NOT NULL,
+	CONSTRAINT archetype_parameter_pk PRIMARY KEY (archetype_id, "name"),
+	CONSTRAINT archetype_parameter_archetype_fk FOREIGN KEY (archetype_id) REFERENCES mocker.archetype(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE mocker.archetype_tag (
+	archetype_id varchar NOT NULL,
+	tag_id varchar NOT NULL,
+	CONSTRAINT archetype_tag_pk PRIMARY KEY (archetype_id,tag_id),
+	CONSTRAINT archetype_tag_archetype_fk FOREIGN KEY (archetype_id) REFERENCES mocker.archetype(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT archetype_tag_tag_fk FOREIGN KEY (tag_id) REFERENCES mocker.resource_tag(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE mocker.archetype_response (
+	id varchar NOT NULL,
+	archetype_id varchar NOT NULL,
+	status int8 NOT NULL,
+	body varchar,
+	CONSTRAINT archetype_response_pk PRIMARY KEY (id),
+	CONSTRAINT archetype_response_archetype_fk FOREIGN KEY (archetype_id) REFERENCES mocker.archetype(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE mocker.archetype_response_header (
+    archetype_response_id varchar NOT NULL,
+    "header" varchar NOT NULL,
+    "value" varchar NOT NULL,
+    CONSTRAINT archetype_response_header_pk PRIMARY KEY (archetype_response_id,"header"),
+    CONSTRAINT archetype_response_header_fk FOREIGN KEY (archetype_response_id) REFERENCES mocker.archetype_response(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE mocker.mock_resource (
+	id int8 NOT NULL,
 	subsystem_url varchar NOT NULL,
 	resource_url varchar NOT NULL,
 	http_method varchar NOT NULL,
 	"name" varchar NOT NULL,
 	is_active boolean NOT NULL,
-	CONSTRAINT mock_resource_pk PRIMARY KEY (id)
+	archetype_id varchar,
+	CONSTRAINT mock_resource_pk PRIMARY KEY (id),
+    CONSTRAINT mock_resource_archetype_fk FOREIGN KEY (archetype_id) REFERENCES mocker.archetype(id) ON DELETE SET NULL ON UPDATE SET NULL
 );
 
 CREATE TABLE mocker.mock_response (
@@ -21,7 +77,7 @@ CREATE TABLE mocker.mock_rule (
 	"name" varchar NOT NULL,
 	"order" int8 NOT NULL,
 	is_active boolean NOT NULL,
-	resource_id varchar NOT NULL,
+	resource_id int8 NOT NULL,
 	response_id varchar NOT NULL,
 	CONSTRAINT mock_rule_pk PRIMARY KEY (id),
 	CONSTRAINT mock_rule_mock_resource_fk FOREIGN KEY (resource_id) REFERENCES mocker.mock_resource(id) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -56,18 +112,12 @@ CREATE TABLE mocker.injectable_parameter (
 	CONSTRAINT injectable_parameter_fk FOREIGN KEY (response_id) REFERENCES mocker.mock_response(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE mocker.tag (
-	id varchar NOT NULL,
-	"value" varchar NOT NULL,
-	CONSTRAINT tag_pk PRIMARY KEY (id)
-);
-
 CREATE TABLE mocker.mock_resource_tag (
-	mock_resource_id varchar NOT NULL,
+	mock_resource_id int8 NOT NULL,
 	tag_id varchar NOT NULL,
 	CONSTRAINT mock_resource_tag_pk PRIMARY KEY (mock_resource_id,tag_id),
 	CONSTRAINT mock_resource_tag_mock_resource_fk FOREIGN KEY (mock_resource_id) REFERENCES mocker.mock_resource(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT mock_resource_tag_tag_fk FOREIGN KEY (tag_id) REFERENCES mocker.tag(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT mock_resource_tag_tag_fk FOREIGN KEY (tag_id) REFERENCES mocker.resource_tag(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE mocker.mock_rule_tag (
@@ -75,5 +125,5 @@ CREATE TABLE mocker.mock_rule_tag (
 	tag_id varchar NOT NULL,
 	CONSTRAINT mock_rule_tag_pk PRIMARY KEY (mock_rule_id,tag_id),
 	CONSTRAINT mock_rule_tag_mock_rule_fk FOREIGN KEY (mock_rule_id) REFERENCES mocker.mock_rule(id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT mock_rule_tag_tag_fk FOREIGN KEY (tag_id) REFERENCES mocker.tag(id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT mock_rule_tag_tag_fk FOREIGN KEY (tag_id) REFERENCES mocker.rule_tag(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
